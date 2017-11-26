b113f652cc9bc100985dea91972bb61169f80b32
ricardough <ricardo.chejfec@mail.mcgill.ca>
2017-11-26 18:29:34 -0500

    Front-End working w parameters

--- create-event.js
+++ create-event.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
     // var wrapper         = $(".name"); //Fields wrapper
 	var rem_button = $("a.remove_field");
 
-    var a = 1;
+    var locs = 1;
+    var times = 1; 
+    var tasks = 1; 
+    var emails = 1; 
 
     $('[data-toggle="tooltip"]').tooltip(); 
 
@@ -12,11 +15,10 @@ $(document).ready(function() {
     	var wrapper = $(this).parent();
         e.preventDefault();
 
-        var str_to_append = '<div class="form-inline"> <br><input type="text" class="form-control" name="locs[' + a + ']" placeholder="Location" required=""> \
-         <button class="btn btn-default remove_field"><i class="fa fa-minus-circle" aria-hidden="true"></i></button></div>';
+        var str_to_append = '<div class="form-inline"> <br><input type="text" class="form-control" name="locs[' + locs + ']" placeholder="Location" required=""> \
+         <button class="locs btn btn-default remove_field"><i class="fa fa-minus-circle" aria-hidden="true"></i></button></div>';
         $(wrapper).append($(str_to_append)); //add input box
-        a++;
-        alert(a);
+        locs++;
     });
 
     $(".add_button_task_guest").click(function(e){ //on add input button click
@@ -24,10 +26,19 @@ $(document).ready(function() {
         e.preventDefault();
         var sibl = $(this).siblings("input");
         var ph = $(sibl).attr("placeholder");
-        var nom = $(sibl).attr("name");
-        alert(nom);
+        var nom = "";
+        var clss = "";
+        if (ph == "Task") {
+            nom = 'tasks[' + tasks + ']';
+            clss = 'tasks';
+            tasks++;
+        } else {
+            nom = 'emails[' + emails + ']';
+            clss = 'emails';
+            emails++;
+        }
         var str_to_append = '<div class="form-inline"> <br><input type="text" class="form-control" name="' + nom + '"  placeholder="'+ ph + '" required="" style="min-width: 30%"> \
-         <button class="btn btn-default remove_field"><i class="fa fa-minus-circle" aria-hidden="true"></i></button></div>';
+         <button class="'+ clss + ' btn btn-default remove_field"><i class="fa fa-minus-circle" aria-hidden="true"></i></button></div>';
         $(wrapper).append($(str_to_append)); //add input box 
 
     });
@@ -37,18 +48,19 @@ $(document).ready(function() {
         e.preventDefault();
         var str_to_append = '<div class="form-inline"> <br> \
                         <div class="form-group"> \
-                            <label for="times[]" class=".sr-only"></label> \
+                            <label for="times['+ (times) +']" class=".sr-only"></label> \
                             <input type="text" class="form-control datepicker" name="times[]" placeholder="Date" required=""> \
                         </div> \
                         <div class="form-group"> \
-                            <label for="times[]" class=".sr-only"></label> \
+                            <label for="times['+ (times) +']" class=".sr-only"></label> \
                             <input type="text" class="form-control timepicker" name="times[]" placeholder="From" required=""> \
                         </div> \
                         <div class="form-group"> \
-                            <label for="times[]" class=".sr-only"></label> \
+                            <label for="times['+ (times) +']" class=".sr-only"></label> \
                             <input type="text" class="form-control timepicker" name="times[]" placeholder="To" required=""> \
                         </div> \
-                        <button class="btn btn-default remove_field"><i class="fa fa-minus-circle" aria-hidden="true"></i></button></div>';
+                        <button class="times btn btn-default remove_field"><i class="fa fa-minus-circle" aria-hidden="true"></i></button></div>';
+        times++;
         $(wrapper).append($(str_to_append)); //add input box
 
     });
@@ -69,6 +81,16 @@ $(document).ready(function() {
 
    
     $(document).on("click",".remove_field", function(e){ //user click on remove text
+        var key = $(this).attr("class").split(" ")[0];
+        if (key == 'tasks') {
+            tasks--;
+        } else if (key == 'emails'){
+            emails--;
+        } else if (key == 'locs'){
+            locs--;
+        }else {
+            times--;
+        }
         e.preventDefault(); $(this).parent('div').remove();
     });
 
